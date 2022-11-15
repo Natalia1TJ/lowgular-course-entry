989e80e80ee1c68f01257157980c74ce354be2a7
nataliatrocha <natalia1tj@gmail.com>
2022-11-15 10:51:32 +0100

    feat: tworzenie serwisu

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {EmployeeModel} from "../../model/employee.model";
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
+import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
@@ -11,7 +12,7 @@ import {EmployeeModel} from "../../model/employee.model";
 })
 export class EmployeeListComponent {
   title: string = 'Tekst, który pojawi się w HTML';
-  constructor(private _httpClient: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
+  constructor(private _employeeService: EmployeeService) { }
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
 }
 
