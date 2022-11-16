29a5b56a47074cc34c875255dfe1c4f0ccdd05c2
nataliatrocha <natalia1tj@gmail.com>
2022-11-16 21:46:43 +0100

    feat: lekcja trzecia display alert with form data

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -13,4 +13,8 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(0), Validators.required]),
     salary: new FormControl(null, [Validators.min(0), Validators.required]),
   });
+
+  onButtonClicked(form: {name:string, age:string, salary:string}) {
+    alert('User was successfully added to the database. Name: ' + form.name + ", Age: " + form.age + ", Salary: " + form.salary);
+  }
 }
