221343f2150bc334d2e388fd93787b86b9137c03
nataliatrocha <natalia1tj@gmail.com>
2022-11-17 20:39:31 +0100

    lekcja trzecia observables

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,6 +4,7 @@ import {EmployeeModel} from "../model/employee.model";
 import {Observable} from "rxjs";
 import {PersonModel} from "../model/person.model";
 import {ProjectModel} from "../model/project.model";
+import {CreateEmployeeModel} from "../model/create-employee.model";
 
 @Injectable()
 export class EmployeeService {
@@ -13,4 +14,8 @@ export class EmployeeService {
   getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>('assets/data/people.json');
   }
+  create(employee: CreateEmployeeModel): Observable<any> {
+    return  this._httpClient.post('url', employee);
+  }
+
 }
